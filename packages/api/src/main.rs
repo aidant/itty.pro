4d677d0d5ffb4d803d334c66e31bcb58b096bc58
aidant <15520814+aidant@users.noreply.github.com>
2024-12-11 17:43:24 +1100

    feat(https): initial prototype of https

--- packages/api/src/main.rs
+++ packages/api/src/main.rs
@@ -4,11 +4,23 @@ use axum::{
     response::IntoResponse,
     Json, Router,
 };
+use hyper::body::Incoming;
+use hyper_util::rt::{TokioExecutor, TokioIo};
 use serde_json::json;
 use sqlx::SqlitePool;
-use std::{env, net::SocketAddr};
+use std::{env, net::SocketAddr, path::PathBuf, sync::Arc};
+use tokio::io::AsyncWriteExt;
 use tokio::net::TcpListener;
+use tokio_rustls::{
+    rustls::{
+        pki_types::{pem::PemObject, CertificateDer, PrivateKeyDer},
+        server::{Acceptor, ClientHello},
+        ServerConfig,
+    },
+    LazyConfigAcceptor,
+};
 use tower_http::trace::TraceLayer;
+use tower_service::Service;
 use tracing::info_span;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
@@ -80,10 +92,75 @@ async fn main() {
         .with_state(AppState { conn });
 
     let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
-    axum::serve(
-        listener,
-        app.into_make_service_with_connect_info::<SocketAddr>(),
-    )
-    .await
-    .unwrap();
+    let mut make_service = app.into_make_service_with_connect_info::<SocketAddr>();
+
+    loop {
+        let (stream, socket) = listener.accept().await.unwrap();
+        println!("socket: {}", socket);
+        let tower_service = make_service.call(socket).await.unwrap();
+
+        tokio::spawn(async move {
+            let acceptor = LazyConfigAcceptor::new(Acceptor::default(), stream);
+            tokio::pin!(acceptor);
+
+            match acceptor.as_mut().await {
+                Ok(start) => {
+                    let client_hello = start.client_hello();
+                    let config = choose_server_config(client_hello).await;
+                    let stream = start.into_stream(config).await.unwrap();
+
+                    let stream = TokioIo::new(stream);
+
+                    let hyper_service =
+                        hyper::service::service_fn(move |request: Request<Incoming>| {
+                            tower_service.clone().call(request)
+                        });
+
+                    hyper_util::server::conn::auto::Builder::new(TokioExecutor::new())
+                        .serve_connection_with_upgrades(stream, hyper_service)
+                        .await
+                        .unwrap();
+                }
+                Err(error) => {
+                    println!("error: {}", error);
+
+                    if let Some(mut stream) = acceptor.take_io() {
+                        stream
+                            .write_all(
+                                format!("HTTP/1.1 400 Invalid Input\r\n\r\n\r\n{:?}\r\n", error)
+                                    .as_bytes(),
+                            )
+                            .await
+                            .unwrap();
+                    }
+                }
+            }
+        });
+    }
+}
+
+pub async fn choose_server_config(client_hello: ClientHello<'_>) -> Arc<ServerConfig> {
+    let sni = client_hello.server_name();
+
+    println!("sni: {:?}", sni);
+
+    let mut config = ServerConfig::builder()
+        .with_no_client_auth()
+        .with_single_cert(
+            CertificateDer::pem_file_iter(
+                PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("localhost-cert.pem"),
+            )
+            .unwrap()
+            .map(|cert| cert.unwrap())
+            .collect(),
+            PrivateKeyDer::from_pem_file(
+                PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("localhost-key.pem"),
+            )
+            .unwrap(),
+        )
+        .unwrap();
+
+    config.alpn_protocols = vec![b"h2".to_vec(), b"http/1.1".to_vec()];
+
+    Arc::new(config)
 }
